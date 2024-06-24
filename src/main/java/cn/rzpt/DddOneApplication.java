96b779b89130134f4757bec963a79e7a6b78e4e5
waxjava04 <waxjava04@163.com>
2024-06-24 23:49:12 +0800

    初始化工程

---
+++ src/main/java/cn/rzpt/DddOneApplication.java
@@ -0,0 +1,13 @@
+package cn.rzpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/rzpt/DddOneApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.rzpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
